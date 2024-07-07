333c3b7331e673de285e08e68cb0d60336a367a1
sanwinmaung <sanwinmaung.dev@gmail.com>
2024-07-07 16:24:13 +0700

    Add Card for Summery

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,18 @@
+import Card from "../ui/dashboard/card/card";
 import styles from "../ui/dashboard/dashboard.module.css";
 
 const Dashboard = () => {
   return (
     <div className={styles.wrapper}>
-      <h1>Dashboard</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.main}>
+          <div className={styles.cards}>
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
