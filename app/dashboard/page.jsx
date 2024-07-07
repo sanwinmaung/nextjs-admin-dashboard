22e99e13e6815b4f40d53e3989148a157cfd6fec
sanwinmaung <sanwinmaung.dev@gmail.com>
2024-07-07 16:19:14 +0700

    Create App, Sidebar with Nav

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import styles from "../ui/dashboard/dashboard.module.css";
+
+const Dashboard = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
