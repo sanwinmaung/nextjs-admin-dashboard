f0fdc44363e08a22fe311cfd652a33c6c298011d
sanwinmaung <sanwinmaung.dev@gmail.com>
2024-07-07 16:22:37 +0700

    Add Navbar

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,5 +1,6 @@
 import Sidebar from "../ui/dashboard/sidebar/sidebar";
 import styles from "../ui/dashboard/dashboard.module.css";
+import Navbar from "../ui/dashboard/navbar/navbar";
 
 const Layout = ({ children }) => {
   return (
@@ -7,7 +8,10 @@ const Layout = ({ children }) => {
       <div className={styles.menu}>
         <Sidebar />
       </div>
-      <div className={styles.content}>{children}</div>
+      <div className={styles.content}>
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 };
